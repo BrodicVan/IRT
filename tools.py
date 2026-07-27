7641ede7b0007eac7fcd6409514e0731b6fd5071
BrodicVan <103325089+BrodicVan@users.noreply.github.com>
2026-07-27 16:24:36 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,15 +1,19 @@
-import random
+import random, torch, jsonlines, json
 
 import numpy as np
 import pandas as pd
-from icecream import ic
 
+from icecream import ic
+from torch.utils.data import Dataset
+from typing import Optional
 from sklearn.cluster import KMeans
-from scipy.stats import spearmanr
+from scipy.stats import spearmanr, pearsonr
 
 
 class Sampler:
-    
+    """
+    数据集采样器，返回原数据集下标的子集。
+    """
     def random_sample(self, number_item, total_count, random_seed=42):
         """
         随机挑选测试集
@@ -45,8 +49,26 @@ class Sampler:
         item_weights = (cluster_sizes / total_count).tolist()
         return item_weights,seen_items,unseen_items
 
+class IRTDataGenerator:
+    """
+    IRT数据生成器，用于批量生成IRT参数纠正器的IRT相关数据。
+    """
+    def __init__(self,responses_jsonl_path:str,,random_seed=42):
+        # 对responses_jsonl_path中的json行进行100轮取5个的抽样
+        self.lines = open(responses_jsonl_path,'r',encoding='utf-8').readlines()
+        self.idxs =  range(len(self.lines))
+        self.random_seed = random_seed
+
+    
+
+        
+        
+
 
 class Evaluator:
+    """
+    压缩后数据集评估器
+    """
     def __init__(self,responses_df:pd.DataFrame):
         self.items = list(responses_df.columns[1:]) # 题目名称
         self.models = responses_df['source'].tolist() # 模型名称
@@ -62,7 +84,6 @@ class Evaluator:
 
         # print(self.sorted_idxs)
 
-
     def sort_idx(self,reduced_items:list[int]):
         """
         计算模型在reduced_items上的平均分数并返回排名
@@ -71,25 +92,26 @@ class Evaluator:
         sorted_idxs_new = reduced_matrix.mean(axis=1).argsort().tolist()
         return sorted_idxs_new
     
-    def spearman(self,reduced_items=list[int]):
+    def spearman(self,reduced_items:list[int]):
         """
         计算模型的reduced_items排名与全集排名的Spearman相关系数
         """
         return spearmanr(self.sorted_idxs,self.sort_idx(reduced_items))
 
-    def pairwise_consistence(self,reduced_items=list[int],delta=2):
+    def pearman(self,reduced_items:list[int]):
+        """
+        计算模型的reduced_items排名与全集排名的Pearman相关系数
+        """
+        return  pearsonr(self.sorted_idxs,self.sort_idx(reduced_items))
+
+    def pairwise_consistence(self,reduced_items:list[int],delta=2):
         """
         计算排名发生变化的模型对数比例，delta表示只统计全集分数相差<delta%的模型对
         """
         
         # 找出全集分数小于delta%的所有模型对
         delta = delta / 100
-        
-        ic(self.scores)
-        ic(self.sorted_idxs)
-
         model_pairs = []
-        
         for i in range(0,self.models_count-1):
             score_i = self.scores[self.sorted_idxs[i]]
             for j in range(i+1,self.models_count):
@@ -98,19 +120,59 @@ class Evaluator:
                     model_pairs.append((self.sorted_idxs[i],self.sorted_idxs[j]))
                 else:
                     break
-        
-        
-        # TODO: 检查排名是否变化
+
+        # 计算压缩数据集的分数、大小顺序、模型排名
+        reduced_scores =  self.responses_matrix[:,reduced_items].mean(axis=1)
+        reduced_sorted_idxs = reduced_scores.argsort()
+        reduced_sorted_idxs = self.sort_idx(reduced_scores)
+        reduced_rank = reduced_sorted_idxs.argmax()
+
+        # 统计排名发生变化的模型对数比例
+        count = 0
         for i,j in model_pairs:
-            pass
-            
+            if reduced_rank[i] > reduced_rank[j]:
+                count += 1
+    
+        return count/len(model_pairs)
+
+class MyDataset(Dataset):
+    """
+    IRT参数纠正器数据，用于生成残差模型的IRT部分训练数据，目前只有离线模式。
+    """
+    def __init__(self,train_IRT_params:Optional[dict]=None, target_item_params:Optional[dict]=None,
+                 item_embeddings:Optional[torch.Tensor]=None, archor_count:int=5,
+                 max_sample_count:int=100000,random_seed:int=42):
+
+        if train_IRT_params is None:
+            raise ValueError('IRTDataloader: 离线生成模式下，需要传入已拟合好的训练IRT参数（题目参数+模型能力）。')
+
+        if target_item_params is None:
+            raise ValueError('IRTDataloader: 需要传入目标题目参数。')
+
+        self.train_IRT_params = train_IRT_params
+        self.target_item_params = target_item_params
+        self.item_embeddings = item_embeddings
+        # TODO: 确定train_IRT_params的格式，从而确定长度
+
+    def __len__(self):
+        return len(self.train_df)
+
+    def __getitem__(self, idx):
+        row = self.train_df.iloc[idx]
+        return {
+            'user_id': row['user_id'],
+            'item_id': row['item_id'],
+            'response': row['response']
+        }
+
+    def get_anchor_items(self):
+        np.random.seed(self.random_seed)
+        anchor_idxs = np.random.choice(len(self.train_df), self.archor_count, replace=False)
+        return self.train_df.iloc[anchor_idxs]
+
+    
 
 
-
-        # print(delta_pairs)
-        # print(delta_pairs)
-        
-            
         
     
 if __name__=='__main__':
@@ -120,7 +182,9 @@ if __name__=='__main__':
     # print(sampler.random_sample(number_item,inputs.shape[0])[0])
     # print(sampler.k_means(number_item, inputs)[0])
 
-    responses_df = pd.read_csv('./resposne_data/processed_data/global_selected_400_test.csv')
-    evaluator = Evaluator(responses_df[:5])
-    evaluator.pairwise_consistence(range(400))
+    # responses_df = pd.read_csv('./resposne_data/processed_data/global_selected_400_test.csv')
+    # evaluator = Evaluator(responses_df[:5])
+    # evaluator.pairwise_consistence(range(400))
+    
+    irt_data_generator = IRTDataGenerator('C:/Users/f50012902/Desktop/IRT-main/demo_train.jsonl')
     
